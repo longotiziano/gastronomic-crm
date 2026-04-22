6b40718933787507c679b1dfb563e75d89efc688
longotiziano <longo.tiziano.arg@gmail.com>
2026-04-21 22:04:42 -0300

    modificacion de la funcion initSelectables para que agregue los parametros del atributo data de botones de clase selectable a la URL

--- webapp/static/js/selectables.js
+++ webapp/static/js/selectables.js
@@ -1,23 +1,36 @@
 /**
  * disables all buttons and selects the clicked one 
+ * 
+ * 
  * @param {Element} clickedBtn 
  * @param {NodeList} allBtns 
  */
 export const selectOption = (clickedBtn, allBtns) => {
-  allBtns.forEach(btn => btn.classList.remove('active'));
-  clickedBtn.classList.add('active');
-  console.log(`Selected button: ${clickedBtn.textContent}`)
+    allBtns.forEach(btn => {btn.classList.remove('active')});
+    clickedBtn.classList.add('active');
+    console.log(`Selected button: ${clickedBtn.textContent}`)
 };
 
 /**
  * applies the selectOption function for the childrens' buttons of every ".selectables" class
+ *
+ * Also, looks for the selectable's data and displays it in the URL, putting just the clicked button's data
  */
 export const initSelectables = () => {
     const selectables = document.querySelectorAll(".selectables");
     selectables.forEach(container => {
+        const dataParam = container.dataset.name;
+        console.log(`Obtained container's data name: ${dataParam}`)
         const btns = container.querySelectorAll("button")
         btns.forEach(btn => {
-            btn.addEventListener("click", () => selectOption(btn, btns))
+            btn.addEventListener("click", () => {
+                selectOption(btn, btns);
+                const url = new URL(window.location.href);
+                const btnData = btn.dataset[dataParam];
+                console.log(`Displaying selectable's value: ${btnData}`);
+                url.searchParams.set(dataParam, btnData);
+                window.history.pushState({}, '', url.toString());   
+            })
         })
     })
 };
