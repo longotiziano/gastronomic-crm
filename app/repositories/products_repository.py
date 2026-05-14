7c8b751bccdc4e8afecd69262b8b5285dc4fa663
Tiziano Longo <longo.tiziano.arg@gmail.com>
2026-05-14 20:44:55 -0300

    Métodos de ProductsRepository eliminados para polimorfismo

--- app/repositories/products_repository.py
+++ app/repositories/products_repository.py
@@ -10,39 +10,3 @@ class ProductsRepository(Repository):
     model = Product
     name = 'product_name'
     id = 'product_id'
-
-    def _count_records(self, name_looked: str = "") -> int:
-        """
-        ### Receives:
-        - A name
-        ### Returns:
-        - The amounts of records that match the received name
-        """
-        try:
-            records = self.session.query(Product).filter(Product.product_name.ilike(f"%{name_looked}%")).count()
-        except SQLAlchemyError as e:
-            raise_and_log("Unexpected server error during the products' records extraction", e, logger)
-        return records
-
-    def get_products(self, r_id: int, actual_offset: int, page_size: int, name_looked: str = "") -> list[dict[str, dict]]:
-        """
-        Returns a list of tuples based on an offset and page size.
-        ### Receives:
-        - Offset
-        - Name looked
-        ### Returns:
-        - List with dictionaries of 
-        """
-        try: # Probability of polimorphism with raw material! # no problem with fstrings cause it's parametized instantly by SQLAlchemy
-            results: list[Product] = self.session.query(Product)\
-                        .filter(Product.product_name.ilike(f"%{name_looked}%"))\
-                        .filter(Product.r_id == r_id)\
-                        .offset(actual_offset)\
-                        .limit(page_size)\
-                        .all()
-        except SQLAlchemyError as e:
-            raise_and_log("Unexpected server error during the products' records extraction", e, logger)
-        if not results:
-            logger.warning("Couldn't find any results while looking for products that match '%s'", name_looked)
-            return [{}]
-        return [prod._to_dict() for prod in results]
